0df3a821c5411d2c22e5a6bf3648c0cd01875f6d
md-azmi-siddique <md.azmi.siddique@gmail.com>
2024-09-30 06:06:21 +0600

    sum of all numbers

--- starPrint2.py
+++ starPrint2.py
@@ -1,23 +1,25 @@
+# def starPrin2(x):
+#     print("Enter the function")
+#     for i in range(1,x,1):
+#         print(" ",end="")
+#         for j in  range(x-1,i,-1):
+#             # print(j)
+#             print(" ",end="")
+#         for k in range(1,i+1,1):
+#             print("*",end="")
+#         print("")
+
 def starPrin2(x):
-    # print("Enter the function")
-    # for i in range(1,x,1):
-    #     print(" ",end="")
-    #     for j in  range(x-1,i,-1):
-    #         # print(j)
-    #         print(" ",end="")
-    #     for k in range(1,i+1,1):
-    #         print("*",end="")
-    #     print("")
-
-    for i in range(1,x+1,1):
-        # print("-",end="")
-        for j in range(x,i,-1):
-            print("-",end="")
-        for k in range (1, i+1, 1):
+    for i in range(x):
+        print("*",end="")
+        # for j in range(x-1,i,-1):
+        #     print("-",end="")
+        # for k in range(1, i+1,1):
+        #     print("*",end="")
+        for l in range(i):
             print("*",end="")
-
+            
         print("")
-        
         
 starPrin2(5)
 
--- sumOfNumbers.py
+++ sumOfNumbers.py
@@ -1,17 +1,24 @@
 import math
 def changeindex(data):
-     if(len(str(data))==3):
-        s = data % 10
-        n = math.floor((data /10)) % 10
-        d = math.floor(((data /10) / 10))% 10
-        reverse = s +n+d
-        return reverse
-     return "Input must be not less or grater that 3 digit"
+   #   if(len(str(data))==3):
+   #      s = data % 10
+   #      n = math.floor((data /10)) % 10
+   #      d = math.floor(((data /10) / 10))% 10
+   #      reverse = s +n+d
+   #      return reverse
+   #   return "Input must be not less or grater that 3 digit"
+   sum = 0
+   while data !=0:
+      eq = data % 10
+      sum = sum + eq
+      data = data // 10
+   return sum
+      
 
-y = int(input())
+y = int(input("Inpuy: "))
 
 z = changeindex(y)
 
-print(z)
+print("Total Sum is: "f"{z}")
 
 

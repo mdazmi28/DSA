3a7c62f561d3c0f0558e77bf318cc064e180b2e2
mdazmi28 <md.azmi.siddique@gmail.com>
2024-09-30 17:24:36 +0600

    delete a position from an array

---
+++ Array/manupulateArray.py
@@ -0,0 +1,12 @@
+lst=[1,2,3,4,5,6,7]
+positon=4
+# # lst=[1,2,4,5,6,7]
+# print(lst)
+for i in range(positon-1,len(lst)-1):
+    # print(i)
+        lst[i] = lst[i+1]
+
+del lst[len(lst)-1]
+# print(x)
+
+print(lst)
---
+++ Array/takeNumberOfInput.py
@@ -0,0 +1,11 @@
+n = int(input("Number of Inputs want to take: "))
+lst=[]
+
+i=0
+while i<n:
+    a = input()
+    lst.append(a)
+    i +=1
+
+print(lst)
+

ca71aeba6ba6f1f6f06efba9b9f9ba8ac0554274
mdazmi28 <md.azmi.siddique@gmail.com>
2024-11-19 11:18:32 +0600

    population and sample cariance

--- Sorting/findVariance.py
+++ Sorting/findVariance.py
@@ -2,13 +2,21 @@ import math
 lst = [1,2,2,3,4,5]
 lenOfLst = len(lst)
 result = 0
+# To calcualte MEAN
 for i in range(len(lst)):
     result = (result + lst[i])
     a = result / lenOfLst
 # print(result)
 print("Mean is: " f"{a:.2f}")
 
-# to calculate variance
+# to calculate variance for popolation
+population_variance_sum = 0
 for i in range(len(lst)):
-    variance = math.pow((lst[i]-a),2)/lenOfLst
-print("Variance is: " f"{variance}")
+    population_variance_sum += (math.pow((lst[i]-a),2))/lenOfLst
+print("Variance of population is: " f"{population_variance_sum:.2f}")
+
+# to calculate variance for sample
+sample_variance_sum = 0
+for i in range(len(lst)):
+    sample_variance_sum += (math.pow((lst[i]-a),2))/(lenOfLst - 1)
+print("Variance of sample is: " f"{sample_variance_sum:.2f}")

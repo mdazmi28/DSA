7b9bea24df7c9a251ec418bf934b5a518775896d
mdazmi28 <md.azmi.siddique@gmail.com>
2024-11-19 15:20:03 +0600

    Standard diviation

--- Sorting/findVariance.py
+++ Sorting/findVariance.py
@@ -14,9 +14,13 @@ population_variance_sum = 0
 for i in range(len(lst)):
     population_variance_sum += (math.pow((lst[i]-a),2))/lenOfLst
 print("Variance of population is: " f"{population_variance_sum:.2f}")
+population_standard_diviation = math.sqrt(population_variance_sum)
+print("Standard Division of population: " f"{population_standard_diviation}")
 
 # to calculate variance for sample
 sample_variance_sum = 0
 for i in range(len(lst)):
     sample_variance_sum += (math.pow((lst[i]-a),2))/(lenOfLst - 1)
 print("Variance of sample is: " f"{sample_variance_sum:.2f}")
+sample_standard_diviation = math.sqrt(sample_variance_sum)
+print("Standard Division of sample: " f"{sample_standard_diviation}")
